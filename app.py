c3013696c589e70096c9ea864696a6929bf5d53f
Toqa Mohamed Ahmed Mahfouz <toqamohammedmahfouz@gmail.com>
2025-09-19 11:27:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,9 @@ st.markdown(
 # ----------------------------
 # Load Model
 # ----------------------------
-model_path = Path("final_model.pkl")
+from pathlib import Path
+model_path = Path("models/final_model.pkl")
+
 
 
 if model_path.exists():
